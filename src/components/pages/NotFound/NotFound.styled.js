df599e11ad5760e01b3b34c3e7a750350ac9aeb4
Olga-Olga <olga_l@ukr.net>
2023-09-08 20:05:28 +0300

    fixed

--- src/components/pages/NotFound/NotFound.styled.js
+++ src/components/pages/NotFound/NotFound.styled.js
@@ -1,12 +1,12 @@
-export const StyledDiv = styled.div`
-  padding: 20px 80px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
+// export const StyledDiv = styled.div`
+//   padding: 20px 80px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+// `;
 
-export const StyledTitle = styled.h2`
-  color: #6150f7;
-  -webkit-text-stroke: 0.2px white;
-  margin-bottom: 5px;
-`;
+// export const StyledTitle = styled.h2`
+//   color: #6150f7;
+//   -webkit-text-stroke: 0.2px white;
+//   margin-bottom: 5px;
+// `;
