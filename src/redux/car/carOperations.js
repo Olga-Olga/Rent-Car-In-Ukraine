d15facbcb82fbb7ee963cef24134cf4170754725
Olga-Olga <olga_l@ukr.net>
2023-09-10 13:01:38 +0300

    Clean

--- src/redux/car/carOperations.js
+++ src/redux/car/carOperations.js
@@ -6,27 +6,6 @@ import 'react-toastify/dist/ReactToastify.css';
 
 axios.defaults.baseURL = 'https://64f6341b2b07270f705e48eb.mockapi.io';
 
-// export const getCarsThunk = createAsyncThunk(
-//   'getCarsThunk',
-//   async (_, { rejectWithValue }) => {
-//     Loading.standard();
-//     try {
-//       const { data } = await axios.get(`/advert`);
-//       Loading.remove();
-//       return data;
-//     } catch (err) {
-//       Loading.remove();
-//       toast.error(
-//         `Sorry, there is an error: ${err.message}. Try to come later. If you see car, they could ba saved from you last visited our platform.`,
-//         {
-//           position: toast.POSITION.TOP_CENTER,
-//         }
-//       );
-//       return rejectWithValue(err.message);
-//     }
-//   }
-// );
-
 export const getCarsThunkPerPage = createAsyncThunk(
   'getCarsThunkPerPage',
   async (page, { rejectWithValue }) => {
@@ -39,7 +18,7 @@ export const getCarsThunkPerPage = createAsyncThunk(
     } catch (err) {
       Loading.remove();
       toast.error(
-        `Sorry, there is an error: ${err.message}. Try to come later. If you see car, they could ba saved from you last visited our platform.`,
+        `Sorry, there is an error: ${err.message}. Try to come later.`,
         {
           position: toast.POSITION.TOP_CENTER,
         }
